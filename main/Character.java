9801ce466051a953757f5afb0ce4111c98839a20
wrynahcalpito <96109858+wrynahcalpito@users.noreply.github.com>
2023-04-05 13:25:06 +0800

    Update Character.java

--- main/Character.java
+++ main/Character.java
@@ -2,10 +2,15 @@ package main;
 
 public abstract class Character {
   protected String name, type;
+  protected int health, maxHealth = 100, energy, maxEnergy = 120, sanity, maxSanity = 100, hunger, maxHunger = 100; 
 
   public Character(String name, String type) { 
     this.name = name;
     this.type = type;
+    health = 100;
+    energy = 120;
+    sanity = 100;
+    hunger = 100;
   }
 
   //GETTER METHODS
@@ -15,9 +20,48 @@ public abstract class Character {
   public String getType() {
     return type;
   }
+      public int getHealth() {
+        return health;
+    }
+    public int getEnergy() {
+        return energy;
+    }
+    public int getSanity() {
+        return sanity;
+    }
+    public int getHunger() {
+        return hunger;
+    } 
+    public int getMaxHealth() {
+        return maxHealth;
+    }
+    public int getMaxEnergy() {
+        return maxEnergy;
+    }
+    public int getMaxSanity() {
+        return maxSanity;
+    }
+    public int getMaxHunger() {
+        return maxHunger;
+    }
   
-  //OPERATION METHODS
-  public void move() {
-    System.out.println("The character has moved.");
-  }
+    //OPERATION METHODS
+    public void setName(String n) {
+        name = n;
+    }
+    public void health() {
+        health -= 1;
+    }
+    public void health(int atk) {
+        health -= 1*atk;
+    }
+    public void sanity() {
+        sanity -= 1;
+    }
+    public void energy() {
+        energy -= 1;
+    }
+    public void hunger() {
+        hunger -= 1;
+    }
 }
