f4371ce18d57e06d4f319e550b5b4289e4bbd864
wrynahcalpito <96109858+wrynahcalpito@users.noreply.github.com>
2023-04-05 13:26:50 +0800

    Update Protagonist.java

--- main/Protagonist.java
+++ main/Protagonist.java
@@ -1,17 +1,13 @@
 package main;
 
 public class Protagonist extends Character {
-    private int health, maxHealth = 100, energy, maxEnergy = 120, sanity, maxSanity = 100, hunger, maxHunger = 100, skillLevel;
+    private int skillLevel;
     private Item inventory[] = {null, null, null, null, null, null, null, null, null};
     private String gallery[] = {null, null, null};
     private Item itemInUse;
 
     public Protagonist(String name) {
         super(name, "Protagonist");
-        health = 100;
-        energy = 120;
-        sanity = 100;
-        hunger = 100;
         skillLevel = 1;
     }
 
@@ -22,54 +18,14 @@ public class Protagonist extends Character {
     public String[] getGallery() {
         return gallery;
     }
-    public int getHealth() {
-        return health;
-    }
-    public int getEnergy() {
-        return energy;
-    }
-    public int getSanity() {
-        return sanity;
-    }
-    public int getHunger() {
-        return hunger;
-    } 
     public int getSkillLevel() {
         return skillLevel;
     }
-    public int getMaxHealth() {
-        return maxHealth;
-    }
-    public int getMaxEnergy() {
-        return maxEnergy;
-    }
-    public int getMaxSanity() {
-        return maxSanity;
-    }
-    public int getMaxHunger() {
-        return maxHunger;
-    }
-
+    
     //OPERATION METHODS
-    public void setName(String n) {
-        name = n;
-    }
-    public void health() {
-        health -= 1;
-    }
-    public void sanity() {
-        sanity -= 1;
-    }
-    public void energy() {
-        energy -= 1;
-    }
-    public void hunger() {
-        hunger -= 1;
-    }
     public void skillLevel() {
         skillLevel += 1;
     }
-    
     public void inspect(Interactive i) {
         i.interact();
     }
