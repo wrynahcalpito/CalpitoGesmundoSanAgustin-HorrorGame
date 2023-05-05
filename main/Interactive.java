3c4c061490454d8cabaa9980fc3d1d06a1926e6b
wrynahcalpito <96109858+wrynahcalpito@users.noreply.github.com>
2023-05-05 22:07:55 +0800

    Update Interactive.java

--- main/Interactive.java
+++ main/Interactive.java
@@ -1,9 +1,13 @@
 package main;
+
 /**
  * Makes objects interactive.
  * 
  * @author TRUTH
  */
 public interface Interactive {
+    /**
+     * Referenced in interact methods in Item class
+     */
     void interact();
 }
