bd4149f7f771648eb08603bc329ebd309d850773
wrynahcalpito <96109858+wrynahcalpito@users.noreply.github.com>
2023-05-05 22:15:43 +0800

    Update Item.java

--- main/Item.java
+++ main/Item.java
@@ -8,34 +8,46 @@ package main;
 import java.util.ArrayList;
 
 public class Item {
-    protected String identifier, type, desc, appearance; //appearance is hardcoded
+    /**
+     * identifier: functions as a name, unique to the item
+     * type: used to reference to a kind of item
+     * appearance: image file name for the appearance of the item
+     */
+    protected String identifier, type, appearance; //appearance is hardcoded
     private static ArrayList<Item> itemList = new ArrayList();
     
-    public Item(String identifier, String type, String desc, String appearance) {
+    /** Initializes the item with parameters
+     * @param identifier
+     * @param type
+     * @param appearance 
+     * Adds the item when declared to the itemList
+     */
+    public Item(String identifier, String type, String appearance) {
         this.identifier = identifier;
         this.type = type;
-        this.desc = desc;
         this.appearance = appearance;
         itemList.add(this);
     }
 
-    //GETTER METHODS
+    /** GETTER METHODS
+     * @return values of variables 
+     */
     public String getIdentifier() {
         return identifier;
     }
     public String getType() {
         return type;
     }
-    public String getDesc() {
-        return desc;
-    } 
     public String getAppearance() {
         return appearance;
     }
 
-    //OPERATION METHODS
+    /**
+     * Collates the information on the item declared
+     * @return information
+     */
     public String interact() {
-        String information = "Item: " + identifier + "%nType: " + type + "%nDescription: " + desc + "%n";
+        String information = "Item: " + identifier + "%nType: " + type + "%n";
         return information;
     }
 }
