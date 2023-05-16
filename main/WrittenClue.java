0a7bfe8e63baac68aad8e0605181abe061abc61a
wrynahcalpito <96109858+wrynahcalpito@users.noreply.github.com>
2023-05-16 11:32:46 +0800

    finished comments & generated javadoc

--- main/WrittenClue.java
+++ main/WrittenClue.java
@@ -1,13 +1,29 @@
 package main;
 
+/**
+ * A subclass of Item. An added variable is clue. 
+ * 
+ * @author TRUTH - CALPITO, GESMUNDO, SAN AGUSTIN
+ */
 public class WrittenClue extends Item {
+    /**
+     * clue: the riddle or any kind of clue that will be used in storage room stage to help the player in the hunt
+     */
     private String clue;
     
-    public WrittenClue(String identifier, String type, String desc, String clue) {
-        super(identifier, "writtenClue", desc, "APieceOfPaperWithString");
+    /**
+     * Initializes the key with parameters
+     * @param identifier acts as the name of the written clue
+     * @param clue the content of the written clue
+     */
+    public WrittenClue(String identifier, String clue) {
+        super(identifier, "writtenClue", null);
         this.clue = clue;
     }
-    
+  
+    /** 
+     * @return clue to be integrated in rectangular layout in main
+     */
     public String printClue() {
         return clue;
     }
