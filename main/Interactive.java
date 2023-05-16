4076f2a5193d9ad4d44997d027fa6ecf79930991
wrynahcalpito <96109858+wrynahcalpito@users.noreply.github.com>
2023-05-16 11:05:53 +0800

    final comments & javadoc generated

--- main/Interactive.java
+++ main/Interactive.java
@@ -3,10 +3,10 @@ package main;
 /**
  * Makes objects interactive.
  * 
- * @author TRUTH
+ * @author TRUTH - CALPITO, GESMUNDO, SAN AGUSTIN
  */
 public interface Interactive {
-    /**
+    /** This is a method that creates interaction between the Protagonist and the Item or NPC
      * Referenced in interact methods in Item class
      */
     void interact();
